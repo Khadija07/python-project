0872ca716b767e908fd466f5f7a5987b28228999
Khadija07 <ktk_01@outlook.com>
2023-06-20 13:05:01 +0300

    3 addition functions added to main.py

--- main.py
+++ main.py
@@ -1,6 +1,51 @@
+import random
+
 MAX_LINES = 3    
 MAX_BET = 100
 MIN_BET = 1
+
+ROWS = 3
+COLS = 3
+
+symbol_count = {
+    "A" : 3,
+    "B" : 4,
+    "C" : 5,
+    "D" : 6
+   
+
+}
+
+#generate items in slot machine
+
+def slot_machine_spin(rows, cols, symbols):
+    all_symbols = []
+    for symbol, symbol_count in symbols.items():
+        for _ in range(symbol_count):
+            all_symbols.append(symbol)
+
+    columns = []
+    for _ in range(cols):
+        column = []
+        current_symbols = all_symbols[:]  #coping the values to current_symbols
+        for _ in range(rows):
+            value = random.choice(current_symbols)
+            current_symbols.remove(value)
+            column.append(value)
+
+        columns.append(column)
+    return columns
+
+def print_slot_machine(columns):
+    for row in range(len(columns[0])):
+        for i, column in enumerate(columns):
+            if i!= len(columns) - 1:
+                print(column[row], end = " | ")   
+            else:
+                print(column[row], end = "")
+
+        print()  #takes to the next line
+
 
 def deposit():
     while True:
@@ -54,4 +99,7 @@ def main():
 
     
     print(f"your total bet for ${bet} on {total_lines} lines is ${total_bet}.")
+
+    slots = slot_machine_spin(ROWS, COLS, symbol_count)
+    print_slot_machine(slots)
 main()
